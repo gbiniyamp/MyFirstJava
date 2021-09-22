b50f378b4639e603cd14ee381d8de82eea3ba25b
gbiniyamp <gbiniyamp@gmail.com>
2021-09-21 22:21:18 -0400

    Initial commit

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -7,5 +7,7 @@ This is the first Java program which will print out a message, Hello world
 public class MyFirstJava {
     public static void main (String args[]){
         System.out.println("Hell world");
+        System.out.println("today is September 20, 2021. I am alive");
+        System.out.println("My name is Biniyam");
     }
 }
